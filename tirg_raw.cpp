558e67fadd2055e8c587cdc5bec98c9a54d8796a
David <kyran36@hotmail.com>
2014-11-09 22:31:07 +0900

    sidestepped some problems with c_str()

--- tirg_raw.cpp
+++ tirg_raw.cpp
@@ -1,24 +1,48 @@
 #include "stdafx.h"
 #include "trg.hpp"
 #include <fstream>
+#include <sstream>
+#include <string>
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 // section added for DLL usage
-DLLEXPORT char* getTextBBoxes(char* path, int w, int h) {
+
+char * toCharArr(std::string s) {
+	static char * converted;
+	delete[] converted;
+	converted = new char[s.length()];
+	for (size_t i = 0; i < s.length(); i++)
+		converted[i] = s[i];
+	return converted;
+}
+
+char * convertResults(std::vector<trg::Rect> r) {
+	// get the results ready for export
+	std::stringstream ss;
+	for (size_t i = 0; i < r.size(); i++){
+		ss << r[i].x1 << "," << r[i].y1 << "," << r[i].x2 << "," << r[i].y2 << ",";
+	}
+	std::string rs = ss.str();
+	rs = rs.substr(0, rs.length() - 1);// remove trailing comma
+	//return (char *)rs.c_str();
+	return toCharArr(rs);
+}
+
+DLLEXPORT char* getTextBBoxesFromFile(char* path, int w, int h) {
 	size_t picWidth = w;
 	size_t picHeight = h;
 	char *picName = path;
 	std::vector<char> buf(picWidth * picHeight * 3);
 
+	// read the file
 	std::filebuf fb;
 	fb.open(path, std::ios_base::in | std::ios_base::binary);
 	fb.sgetn((char*)&buf[0], buf.size() * sizeof(buf[0]));
 
-	//std::vector<std::vector<trg::Rgb> > a(picHeight, picWidth);
+	// do the image processing
 	std::vector<std::vector<trg::Rgb>> a (picHeight, std::vector<trg::Rgb>(picWidth, {0, 0, 0}));
-
 	for (size_t y = 0; y < picHeight; ++y) {
 	    for (size_t x = 0; x < picWidth; ++x) {
 	        size_t pos = 3 * y * picWidth + 3 * x;
@@ -28,7 +52,7 @@ DLLEXPORT char* getTextBBoxes(char* path, int w, int h) {
 	}
 	std::vector<trg::Rect> r = trg::get_textlike_regions(a);
 
-	return path;
+	return convertResults(r);
 }
 
 DLLEXPORT char* getTextBBoxesFromBytes(char* b, int w, int h) {
@@ -43,50 +67,8 @@ DLLEXPORT char* getTextBBoxesFromBytes(char* b, int w, int h) {
 	}
 	std::vector<trg::Rect> r = trg::get_textlike_regions(a);
 
-	return "DLL function not finished yet";
+	return convertResults(r);
 }
 
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
-int main1() {
-
-	const size_t picWidth = 300;
-	const size_t picHeight = 225;
-	const char *picName = "222t.png";
-
-	char buf[picWidth * picHeight * 3];
-	FILE *fp;
-	errno_t errorCode = fopen_s(&fp, picName, "rb");
-	fread(buf, 1, sizeof(buf), fp);
-	fclose(fp);
-
-	//std::vector<std::vector<trg::Rgb> > a(picHeight, picWidth);
-	std::vector<std::vector<trg::Rgb>> a(picHeight, std::vector<trg::Rgb>(picWidth, { 0, 0, 0 }));
-
-	for (size_t y = 0; y < picHeight; ++y) {
-		for (size_t x = 0; x < picWidth; ++x) {
-			size_t pos = 3 * y * picWidth + 3 * x;
-			trg::Rgb pixel = { buf[pos], buf[pos + 1], buf[pos + 2] };
-			a[y][x] = pixel;
-		}
-	}
-
-	std::vector<trg::Rect> r = trg::get_textlike_regions(a);
-
-	puts("====================================");
-	puts(picName);
-	puts("====================================");
-	printf("Detected %d region(s):\n", r.size());
-	puts("====================================");
-	if (!r.empty()) {
-		for (size_t i = 0; i < r.size(); ++i) {
-			printf("%3d. (%d, %d) (%d, %d)\n", i + 1, r[i].x1, r[i].y1, r[i].x2, r[i].y2);
-		}
-	}
-	else
-		printf("...Nothing to report...\n");
-	puts("====================================");
-	puts("Press any key to exit...");
-	getchar();
-	return 0;
-}
